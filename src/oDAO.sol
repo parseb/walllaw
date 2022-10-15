2e85e5982cdda202b3843debc4437186a72ac5be
parseb <petra306@protonmail.com>
2022-10-15 19:20:15 +0300

    init

--- src/oDAO.sol
+++ src/oDAO.sol
@@ -1,14 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+contract oDAO {
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
 
-    function increment() public {
-        number++;
-    }
+
 }
